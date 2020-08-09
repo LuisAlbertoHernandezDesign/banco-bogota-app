09bb5731199e3164432ad046c1934b9ba9531314
Luis Hernandez <luis.hernandez1404@gmail.com>
2020-08-09 17:13:23 -0500

    v1.0.0 Se Agrego Header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'banco-bogota-app';
+  title = 'Banco de Bogota';
+  herramientas: string= 'Spring con Angular 7';
 }
